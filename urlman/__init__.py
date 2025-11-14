8b49c48d70493fe4f3a68ce1ca01ff3e9d64236f
Tobias Kunze <r@rixx.de>
2025-11-14 23:54:42 +0100

    Set attributes used by pydoc/inspect (#20)
    Closes #19

--- urlman/__init__.py
+++ urlman/__init__.py
@@ -45,6 +45,9 @@ class Urls(metaclass=UrlsMetaclass):
         self.context = {"self": self.instance}
         self.context.update(self.urls)
         self.__qualname__ = ".".join((klass.__qualname__, name))
+        self.__objclass__ = klass
+        self.__name__ = name
+        self.__wrapped__ = None
 
     def __getattr__(self, attr):
         return self.get_url(attr)
